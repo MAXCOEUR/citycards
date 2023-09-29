07514c833e68eb6ef2d7c1d64bc88e278065c5c9
MAXCOEUR <45947596+MAXCOEUR@users.noreply.github.com>
2023-09-29 16:59:38 +0200

    creation class CreateUser

--- app/src/main/java/com/example/citycards/Model/User.kt
+++ app/src/main/java/com/example/citycards/Model/User.kt
@@ -1,3 +1,10 @@
 package com.example.citycards.Model
 
-class User(id: Int, username: String, password: String, Avatar: String, Jeton: Int)
+data class User(val id: Int, val username: String, val email:String, val password: String, val avatar: String, var Jeton: Int)
+
+data class CreateUser(
+    var username: String? = null,
+    var email: String? = null,
+    var password: String? = null,
+    var avatar: String? = null
+)
