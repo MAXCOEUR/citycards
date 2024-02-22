1b494e9d08c7289ed35a952bab5bbc33788285c7
Maxence COEUR <maxence.coeur@etu.univ-lyon1.fr>
2024-02-22 14:57:32 +0100

    bug image picker

--- app/src/main/java/com/example/citycards/View/CreateUser/AvatarFragment.kt
+++ app/src/main/java/com/example/citycards/View/CreateUser/AvatarFragment.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -117,18 +118,57 @@ class AvatarFragment : Fragment() {
             else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
-    fun imagePicker(){
-        val permission = Manifest.permission.READ_EXTERNAL_STORAGE
-        if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
-            // Si la permission n'a pas été accordée, demandez-la à l'utilisateur
-            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_STORAGE_PERMISSION)
-        } else {
-            // Si la permission a déjà été accordée, lancez l'intent pour sélectionner une image
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-                type = "image/*"
-                addCategory(Intent.CATEGORY_OPENABLE)
+    fun imagePicker() {
+        val permissionImage = Manifest.permission.READ_MEDIA_IMAGES
+        val permissionVideo = Manifest.permission.READ_MEDIA_VIDEO
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        permissionImage
+                    )
+                } != PackageManager.PERMISSION_GRANTED ||
+                context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        permissionVideo
+                    )
+                } != PackageManager.PERMISSION_GRANTED) {
+                // Si la permission n'a pas été accordée, demandez-la à l'utilisateur
+                requestPermissions(
+                    arrayOf(permissionImage, permissionVideo),
+                    REQUEST_STORAGE_PERMISSION
+                )
+            } else {
+                // Si la permission a déjà été accordée, lancez l'intent pour sélectionner une image
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    type = "image/*"
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                }
+                startActivityForResult(intent, REQUEST_IMAGE_OPEN)
             }
-            startActivityForResult(intent, REQUEST_IMAGE_OPEN)
+        } else {
+            val permission = Manifest.permission.READ_EXTERNAL_STORAGE
+            if (context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        permission
+                    )
+                } != PackageManager.PERMISSION_GRANTED) {
+                // Si la permission n'a pas été accordée, demandez-la à l'utilisateur
+                requestPermissions(
+                    arrayOf(permission),
+                    REQUEST_STORAGE_PERMISSION
+                )
+            } else {
+                // Si la permission a déjà été accordée, lancez l'intent pour sélectionner une image
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    type = "image/*"
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                }
+                startActivityForResult(intent, REQUEST_IMAGE_OPEN)
+            }
         }
     }
 
--- app/src/main/java/com/example/citycards/View/Profile/UserFragment.kt
+++ app/src/main/java/com/example/citycards/View/Profile/UserFragment.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -57,6 +58,7 @@ class UserFragment : Fragment() {
 
         val username = view.findViewById<TextView>(R.id.tv_userName)
         val email = view.findViewById<TextView>(R.id.tv_Email)
+        val nbJeton = view.findViewById<TextView>(R.id.tv_nbjeton)
 
         val btnSave = view.findViewById<Button>(R.id.bt_Save)
         val btnChgMdp = view.findViewById<Button>(R.id.bt_ChgMdp)
@@ -72,6 +74,7 @@ class UserFragment : Fragment() {
 
         username.text = UserRepository.getUserLogin().username
         email.text = UserRepository.getUserLogin().email
+        nbJeton.text = UserRepository.getUserLogin().token.toString()
 
         avatar.setOnClickListener {
             imagePicker()
@@ -124,27 +127,59 @@ class UserFragment : Fragment() {
 
 
     fun imagePicker() {
-        val permission = Manifest.permission.READ_EXTERNAL_STORAGE
-        if (context?.let {
-                ContextCompat.checkSelfPermission(
-                    it,
-                    permission
+        val permissionImage = Manifest.permission.READ_MEDIA_IMAGES
+        val permissionVideo = Manifest.permission.READ_MEDIA_VIDEO
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        permissionImage
+                    )
+                } != PackageManager.PERMISSION_GRANTED ||
+                context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        permissionVideo
+                    )
+                } != PackageManager.PERMISSION_GRANTED) {
+                // Si la permission n'a pas été accordée, demandez-la à l'utilisateur
+                requestPermissions(
+                    arrayOf(permissionImage, permissionVideo),
+                    REQUEST_STORAGE_PERMISSION
                 )
-            } != PackageManager.PERMISSION_GRANTED) {
-            // Si la permission n'a pas été accordée, demandez-la à l'utilisateur
-            requestPermissions(
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                REQUEST_STORAGE_PERMISSION
-            )
-        } else {
-            // Si la permission a déjà été accordée, lancez l'intent pour sélectionner une image
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-                type = "image/*"
-                addCategory(Intent.CATEGORY_OPENABLE)
+            } else {
+                // Si la permission a déjà été accordée, lancez l'intent pour sélectionner une image
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    type = "image/*"
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                }
+                startActivityForResult(intent, REQUEST_IMAGE_OPEN)
             }
-            startActivityForResult(intent, REQUEST_IMAGE_OPEN)
+        } else {
+            val permission = Manifest.permission.READ_EXTERNAL_STORAGE
+            if (context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        permission
+                    )
+                } != PackageManager.PERMISSION_GRANTED) {
+                // Si la permission n'a pas été accordée, demandez-la à l'utilisateur
+                requestPermissions(
+                    arrayOf(permission),
+                    REQUEST_STORAGE_PERMISSION
+                )
+            } else {
+                // Si la permission a déjà été accordée, lancez l'intent pour sélectionner une image
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    type = "image/*"
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                }
+                startActivityForResult(intent, REQUEST_IMAGE_OPEN)
+            }
         }
     }
+
 
     companion object {
         @JvmStatic
